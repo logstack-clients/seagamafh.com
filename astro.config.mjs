3326fb314bdfaac7e622d1d9e33046aa288b84de
xm3d <muhammedasm00@gmail.com>
2026-06-08 22:12:51 +0000

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import { publicEnv } from "./src/env/public";
 // https://astro.build/config
 export default defineConfig({
     site: publicEnv.SITE_BASE_URL,
-    integrations: [react()]
+    integrations: [react()],
+    build: {
+        inlineStylesheets: 'auto',
+        assets: 'strict',
+    },
+    output: 'static',
 });
