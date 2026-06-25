29f02d371fdf0fb791a400dc7d3a341e7e4aec9a
xm3d <muhammedasm00@gmail.com>
2026-06-25 20:51:29 +0000

    deleted the postcss config and added vite configs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import { publicEnv } from "./src/env/public";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,9 @@ export default defineConfig({
     build: {
         inlineStylesheets: 'auto',
         assets: 'strict',
+    },
+    vite: {
+        plugins: [tailwindcss()],
     },
     output: 'static',
 });
--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-const config = {
-    plugins: {
-        "@tailwindcss/postcss": {},
-        autoprefixer: {},
-        cssnano: { preset: 'default' }
-    },
-};
-
-export default config;
